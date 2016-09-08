296966351e6a16bc932be81d32a3244b8193c799
jorgicor <jorge.giner@hotmail.com>
2016-09-08 16:36:32 +0200

    * Changed copyrights to Jorge Giner.

--- timer.asm
+++ timer.asm
@@ -1,7 +1,5 @@
 #if 0
-The MIT License (MIT)
-
-Copyright (c) 2014 inmensabolademanteca@gmail.com
+Copyright (c) 2014, 2016 Jorge Giner Cordero
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
